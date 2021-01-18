32d349fb2a288501a8e58db03fffcddf36ee8b3a
tiotule <77607736+tiotule@users.noreply.github.com>
2021-01-18 10:59:08 +0100

    Add files via upload

---
+++ program hackujący.py
@@ -0,0 +1,11 @@
+import random
+print("wpisz cztero literowy kod aby go zhakować:")
+hacks = random.randint(1000, 9000)
+number_input = input()
+number_input = int(number_input)
+kod = number_input
+while hacks != kod:
+    hacks = random.randint(1000, 9000)
+    print(hacks)
+    if hacks == kod:
+        print("gotowe!")
